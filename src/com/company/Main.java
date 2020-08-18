42d133340e09aac207fe1a743dcbfd0ade6636a9
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-18 22:00:42 +0300

    learned about variable types

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+	    // write your code here
+        byte num = 23; // smol number (-128, 127)
+        int num2 = 24; // kinda big number (-2**31, 2**31-1)
+        float num3 = 25.123f; // floating point
+        double num4 = 56.12; // bigger floating point
+        boolean isTrue = true; // truth or LIES
+        char character = 'r'; // a character
+        String str = "lololol"; // more characters
+        System.out.println(str); // prints stuff out
+    }
+}
