b97f5766a6b0254333db005df69a0b1cceeab804
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-19 21:42:12 +0300

    learned how to take user input from console with Scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,11 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 	    // write your code here
-        byte num = 23; // smol number (-128, 127)
-        int num2 = 24; // kinda big number (-2**31, 2**31-1)
-        float num3 = 25.123f; // floating point
-        double num4 = 56.12; // bigger floating point
-        boolean isTrue = true; // truth or LIES
-        char character = 'r'; // a character
-        String str = "lololol"; // more characters
-        System.out.println(str); // prints stuff out
+        Scanner str = new Scanner(System.in);
+        System.out.println(str.nextLine());
     }
 }
