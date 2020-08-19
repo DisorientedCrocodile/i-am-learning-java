8b56efab0958d961d84bc4f81b7c61aab2d832d2
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-19 22:23:38 +0300

    learned about if, else, else if, switch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,31 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
         Scanner num = new Scanner(System.in); // a Scanner object
-        int first, second, result;
-        System.out.println("Enter first num: ");
-        first = num.nextInt(); // takes an int as input
-        System.out.println("Enter second num: ");
-        second = num.nextInt(); // same here
-        result = first + second; // can be + - * / or %
-        // if i wanted to add an int to another int i could do result += first
-        // there also are -= *= /=
-        // if i want to add 1 i can do result++;
-        // if i need to subtract 1 i could do result--;
-        System.out.println("The result is: " + result);
+        int first, second = 50;
+        System.out.println("Enter 1st num: ");
+        first = num.nextInt();
+
+        if (first > second) { // found out it actually works without brackets if its only one line lol
+            System.out.println("bigger than 50");
+            System.out.println("\n");
+        } else if (first == second) {
+            System.out.println("same as 50");
+        }
+        else {
+            System.out.println("lower than 50");
+        }
+
+        switch (first) {
+            case 51:
+            case 123:
+                System.out.println(first);
+                break;
+            case 56:
+                System.out.println("56 lol");
+            default:
+                System.out.println("not 51 123 or 56");
+        }
+
+        // not much since it's pretty much the same with c-sharp
     }
 }
