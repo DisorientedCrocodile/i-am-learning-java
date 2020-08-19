350dc66adf801a7d7fcd400c960905d6d3972100
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-19 21:55:35 +0300

    learned about math operations (pretty much the same as python or c-sharp)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 	    // write your code here
-        Scanner str = new Scanner(System.in);
-        System.out.println(str.nextLine());
+        Scanner num = new Scanner(System.in); // a Scanner object
+        int first, second, result;
+        System.out.println("Enter first num: ");
+        first = num.nextInt(); // takes an int as input
+        System.out.println("Enter second num: ");
+        second = num.nextInt(); // same here
+        result = first + second; // can be + - * / or %
+        // if i wanted to add an int to another int i could do result += first
+        // there also are -= *= /=
+        // if i want to add 1 i can do result++;
+        // if i need to subtract 1 i could do result--;
+        System.out.println("The result is: " + result);
     }
 }
