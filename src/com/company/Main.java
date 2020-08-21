d064e04cc9223501c341a29e711f425bd7fd6fd0
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-21 22:19:13 +0300

    learned about loops in java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,27 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-	    // write your code here
-        Scanner num = new Scanner(System.in); // a Scanner object
-        int first, second = 50;
-        System.out.println("Enter 1st num: ");
-        first = num.nextInt();
-
-        if (first > second) { // found out it actually works without brackets if its only one line lol
-            System.out.println("bigger than 50");
-            System.out.println("\n");
-        } else if (first == second) {
-            System.out.println("same as 50");
-        }
-        else {
-            System.out.println("lower than 50");
+        // write your code here
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
 
-        switch (first) {
-            case 51:
-            case 123:
-                System.out.println(first);
-                break;
-            case 56:
-                System.out.println("56 lol");
-            default:
-                System.out.println("not 51 123 or 56");
+        int j = 1;
+        while(j <= 10) {
+            System.out.println(j);
+            j++;
+            if (j == 5) {
+                break; // gets out of the loop (there's also return)
+                // continue; can skip an iteration of a loop
+            }
         }
 
-        // not much since it's pretty much the same with c-sharp
+        int x = 0;
+        do { // quote from portal 2: "They said i couldn't buy 7 dollars worth of moon rocks much less 70 million.
+            //  BOUGHT 'EM ANYWAY"
+            x++;
+            System.out.println(x);
+        } while (x < 0);
     }
 }
