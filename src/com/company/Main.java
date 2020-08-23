c7c64952b74f45670526a6c24235da3953c0723c
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-23 21:42:04 +0300

    learned about arrays in java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        int[] nums;
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how long you want the array to be: ");
+        n = scanner.nextInt();
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter nums[" + i + "]");
+            nums[i] = scanner.nextInt();
         }
-
-        int j = 1;
-        while(j <= 10) {
-            System.out.println(j);
-            j++;
-            if (j == 5) {
-                break; // gets out of the loop (there's also return)
-                // continue; can skip an iteration of a loop
-            }
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + ". " + nums[i]);
         }
-
-        int x = 0;
-        do { // quote from portal 2: "They said i couldn't buy 7 dollars worth of moon rocks much less 70 million.
-            //  BOUGHT 'EM ANYWAY"
-            x++;
-            System.out.println(x);
-        } while (x < 0);
+        // i know VEERYY UsEfUl
     }
 }
