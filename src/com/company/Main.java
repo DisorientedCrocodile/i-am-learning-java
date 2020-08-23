735cfab20bb4f2904fb258317d158ffc02033a98
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-23 22:02:46 +0300

    learned about functions in java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,15 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-        int[][] array = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                array[i][j] = i + j; // idk i just put a random thing here
-                System.out.println(array[i][j]);
-            }
+        //write your code here
+        int temp = func(8,2);
+        System.out.println(temp);
+    }
+    public static int func(int a, int b) {
+        int res = 1;
+        for (int i = 0; i < b; i++) {
+            res *= a;
         }
+        return res;
     }
 }
