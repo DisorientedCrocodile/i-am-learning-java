de4734c79190d1bc153ea56c816cdb911539c906
DisorientedCrocodile <xeoplay123456@gmail.com>
2020-08-24 21:21:05 +0300

    learned about oop in java and made new class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         //write your code here
-        int temp = func(8,2);
-        System.out.println(temp);
-    }
-    public static int func(int a, int b) {
-        int res = 1;
-        for (int i = 0; i < b; i++) {
-            res *= a;
-        }
-        return res;
+        Person dude = new Person();
+        System.out.println(dude.height);
+        System.out.println(dude.weight);
+        dude.say("lol");
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Person {
+    public int height = 180;
+    public int weight = 60;
+
+    public void say(String msg) {
+        System.out.println("The message is: " + msg);
+    }
+}
